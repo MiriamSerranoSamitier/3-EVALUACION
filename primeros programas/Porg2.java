7b0d9626b5d85957833f67cc21b69d702ad49102
MiriamSerranoSamitier <113890487+MiriamSerranoSamitier@users.noreply.github.com>
2023-04-23 16:25:58 +0200

    Create Porg2.java

---
+++ primeros programas/Porg2.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Prog2 {
+    public static void main(String[] args) {
+        System.out.println("Hola, bienvenido al programa 2 (muy guay)");
+        int num1;
+        int num2;
+        num1 = 1;
+        num2 = 2;
+        System.out.print("Resultado--> ");
+        System.out.println(num1 + num2);
+    }
+}
