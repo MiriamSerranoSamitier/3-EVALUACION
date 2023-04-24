78a18507fea9f593df2f080b58d276140f5f3e3e
MiriamSerranoSamitier <113890487+MiriamSerranoSamitier@users.noreply.github.com>
2023-04-24 14:02:51 +0200

    Delete primeros programas directory

--- primeros programas/Prog1.java
+++
@@ -1,5 +0,0 @@
-public class Prog1{
-    public static void main(String[] args){
-        System.out.print("Hola, Antonio, López!");
-    }
-}
--- primeros programas/Prog4.java
+++
@@ -1,9 +0,0 @@
-package PrimerasClases;
-public class Prog4 {
-    public static void main(String[] args){
-        Punto pto;
-        pto= new Prog3(2,3);
-        System.out.println("Este punto dista del origen--> ");
-        System.out.println(pto.devuelveDistanciaOrigen());
-    }
-}
